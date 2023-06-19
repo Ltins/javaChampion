8b78c36688b124f72ee9f2b3ca8fe570c71b05a2
LTINS <adondenko@bk.ru>
2023-06-19 14:41:40 +0300

    Added connection to JDBC driver

--- src/main/java/org/ltins/Main.java
+++ src/main/java/org/ltins/Main.java
@@ -1,7 +1,32 @@
 package org.ltins;
 
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
+    public static final String USER_NAME = "root";
+    public static final String URL = "jdbc:mysql://localhost:3306/oleg";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input your password: ");
+            String password = in.nextLine();
+            in.close();
+
+            Connection connection = DriverManager.getConnection(URL, USER_NAME, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from providers");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("country_of_origin"));
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 }
