91244cc6a6d7327cf284b0bbeea448adae7fd3cf
LTINS <adondenko@bk.ru>
2023-06-20 12:23:49 +0300

    Small Fix

--- src/main/java/org/ltins/Main.java
+++ src/main/java/org/ltins/Main.java
@@ -7,13 +7,12 @@ import DataBase.DBConnection;
 
 public class Main {
     public static void main(String[] args) {
-//            Scanner in = new Scanner(System.in);
-//            System.out.print("Input your password: ");
-//            String password = in.nextLine();
-//            in.close();
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input your password: ");
+            String password = in.nextLine();
+            in.close();
 
-            //DBConnection oleg = new DBConnection(password);
-            DBConnection oleg = new DBConnection("root","18422184", "oleg");
+            DBConnection oleg = new DBConnection("root",password, "oleg");
 
             oleg.printTables();
             oleg.printTable("employees");
