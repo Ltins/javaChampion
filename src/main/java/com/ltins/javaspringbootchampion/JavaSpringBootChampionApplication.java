6141eb0941acdf083dd642c8068826bc1809776a
LTINS <adondenko@bk.ru>
2023-06-22 10:30:44 +0300

    Spring init

---
+++ src/main/java/com/ltins/javaspringbootchampion/JavaSpringBootChampionApplication.java
@@ -0,0 +1,13 @@
+package com.ltins.javaspringbootchampion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootChampionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootChampionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ltins/javaspringbootchampion/JavaSpringBootChampionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltins.javaspringbootchampion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootChampionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
